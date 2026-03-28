05de65c517fea8cc232ccb319aac75581c258dc2
efemirik <efemirik05@gmail.com>
2026-03-28 21:14:48 +0300

    Day 1: Initial project setup and folder structure

---
+++ cmd/api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Starting Guardian Auth API...")
+	log.Println("Server is getting ready to listen...")
+	// TODO: Initialize Database
+	// TODO: Setup Gin Router
+	// TODO: Start Server
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/efemirik/guardian-auth-api.git
+
+go 1.26.1
